b0d6c8544902960b3aa8ac4b9fcf8bd36f3fec5b
vagrant <vagrant@homestead>
2018-09-25 14:36:11 +0000

    inport payment library

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //inject 'alipay' instance into container
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+
+            //check whether this project operation env is 'production'
+            if (app()->environment() !== 'production') {
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //call Yansongda\Pay to create a alipay object
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //call Yansongda\Pay to create a wechat project
+            return Pay::wechat($config);
+        });
     }
 }
